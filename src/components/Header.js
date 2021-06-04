1baf0ca435cd365da0234ac4c3bd540de1db67a6
Yunus Emre Halis <yemrehalis@gmail.com>
2021-06-04 18:27:28 +0300

    hamberger menu finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
 
 function Header() {
+  const [burgerStatus, setBurgerStatus] = useState(false);
+
   return (
     <Container>
       <a href="#">
@@ -16,10 +19,43 @@ function Header() {
       <RightMenu>
         <a href="#">Shop</a>
         <a href="#">Tesla Account</a>
-        <MenuIconContainer>
+        <MenuIconContainer onClick={() => setBurgerStatus(true)}>
           <MenuIcon />
         </MenuIconContainer>
       </RightMenu>
+      <BurgerNav status={burgerStatus}>
+        <CloseContainer>
+          <Close onClick={() => setBurgerStatus(false)} />
+        </CloseContainer>
+
+        <li>
+          <a href="#">Model S</a>
+        </li>
+        <li>
+          <a href="#">Model Y</a>
+        </li>
+        <li>
+          <a href="#">Exisiting Inventory</a>
+        </li>
+        <li>
+          <a href="#">Used Inventory</a>
+        </li>
+        <li>
+          <a href="#">Trade-in</a>
+        </li>
+        <li>
+          <a href="#">Cybertruck</a>
+        </li>
+        <li>
+          <a href="#">Roadster</a>
+        </li>
+        <li>
+          <a href="#">Semi</a>
+        </li>
+        <li>
+          <a href="#">Charging</a>
+        </li>
+      </BurgerNav>
     </Container>
   );
 }
@@ -36,6 +72,7 @@ const Container = styled.div`
   top: 0;
   left: 0;
   right: 0;
+  z-index: 10;
 `;
 
 const MenuGroup = styled.div`
@@ -62,4 +99,37 @@ const MenuIconContainer = styled.div`
   display: flex;
   align-items: center;
   cursor: pointer;
+`;
+
+const CloseContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const Close = styled(CloseIcon)`
+  cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  position: fixed;
+  width: 300px;
+  background: white;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  padding: 20px;
+  list-style: none;
+  transform: ${(props) =>
+    props.status ? "translateX(0%)" : "translateX(100%)"};
+
+  transition: transform 0.3s ease-in;
+
+  li {
+    padding: 15px 0;
+    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+
+    a {
+      font-weight: 600;
+    }
+  }
 `;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,16 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 
-function Section() {
+function Section(props) {
+  console.log(props);
   return (
-    <Container>
+    <Container bgImage={props.backgroundImg}>
       <ItemText>
-        <h1>Model S</h1>
-        <p>Order Online for Toucless Delivery</p>
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
       </ItemText>
       <ButtonGroup>
-        <LeftButton>Custom Order</LeftButton>
-        <RightButton>Existing Inventory</RightButton>
+        <LeftButton>{props.leftBtnText}</LeftButton>
+        <RightButton> {props.rightBtnText} </RightButton>
       </ButtonGroup>
       <DownArrow src="./images/down-arrow.svg" />
     </Container>
@@ -20,10 +21,9 @@ function Section() {
 export default Section;
 
 const Container = styled.div`
-  background-color: blue;
   width: 100%;
   height: 100vh;
-  background-image: url("./images/model-s.jpg");
+  background-image: ${(props) => `url('./images/${props.bgImage}')`};
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
@@ -68,4 +68,5 @@ const RightButton = styled(LeftButton)`
 const DownArrow = styled.img`
   height: 40px;
   animation: animateDown infinite 1.5s;
+  margin-bottom: 50px;
 `;
